4dd59b51ff7314ae96dc22ce46485bf38f35edca
Kiyaaen <kikinaqvi97@gmail.com>
2025-09-26 17:48:29 +0100

    Update receiver.js

--- Scripts/reader/receiver.js
+++ Scripts/reader/receiver.js
@@ -702,7 +702,7 @@ function startBailao() {
   });
   document.body.appendChild(overlay);
 
-  bailaoAudio = new Audio("https://github.com/KikiNaqvi/Sparx/blob/main/media/MONTAGEM%20BAILÃO%20(Sped%20Up).mp3");
+  bailaoAudio = new Audio("https://github.com/KikiNaqvi/Sparx/raw/main/media/MONTAGEM%20BAILÃO%20(Sped%20Up).mp3");
   bailaoAudio.loop = true;
   bailaoAudio.volume = 0.7;
   bailaoAudio.play().catch(() => console.warn("bailao audio blocked"));
