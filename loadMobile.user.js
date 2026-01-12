6eae9290919addbf9cfb50caffd857aaa444d75b
Kiyaaen <kikinaqvi97@gmail.com>
2026-01-12 00:50:08 +0000

    Update loadMobile.user.js

--- loadMobile.user.js
+++ loadMobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SparxCheat
 // @namespace    https://itskiyan.xyz/
-// @version      1.0
+// @version      1.0.1
 // @description  Loads external scripts based on site
 // @match        *://*.sparxmaths.uk/*
 // @match        *://maths.sparx-learning.com/*
@@ -11,6 +11,8 @@
 // @match        *://science.sparx-learning.com/*
 // @run-at       document-end
 // @grant        none
+// @updateURL    https://www.itskiyan.xyz/loadMobile.user.js
+// @downloadURL  https://www.itskiyan.xyz/loadMobile.user.js
 // ==/UserScript==
 
 (async()=>{const b="https://www.itskiyan.xyz/Scripts/",u=location.href,m={js:["maths/maths.js"]},r={js:["reader/reader.js"],css:[]},s={js:["science/science.js","science/inject.js","maths/deps/h2c.js"],css:[]};let f={js:[],css:[]};if(u.includes("sparxmaths.uk")||u.includes("maths.sparx-learning.com"))f=m;else if(u.includes("app.sparxreader.com")||u.includes("sparxreader.com")||u.includes("reader.sparx-learning.com"))f=r;else if(u.includes("science.sparx-learning.com"))f=s;if(!document.body)await new Promise(r=>document.addEventListener("DOMContentLoaded",r));(f.css||[]).forEach(c=>{const l=document.createElement("link");l.rel="stylesheet";l.href=b+c;document.head.appendChild(l)});for(const j of f.js)try{await new Promise((r,e)=>{const s=document.createElement("script");s.src=b+j;s.onload=r;s.onerror=e;document.body.appendChild(s)})}catch(err){console.error("Failed to load",j,err)}if(f===r)try{await new Promise((r,e)=>{const s=document.createElement("script");s.src=b+"reader/receiver.js";s.onload=r;s.onerror=e;document.body.appendChild(s)})}catch(err){console.error("Failed to load receiver.js",err)}})();
